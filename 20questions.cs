6763949bb796285ab319ce5b75e683be2e14f0c6
chc232u07 <chc232u07@Instruction.network.bcc>
2019-09-17 20:06:51 -0700

    added more to it

--- 20questions.cs
+++ 20questions.cs
@@ -1,6 +1,6 @@
 //Travis Kroll
 //9-17-19
-//This program will ask you a series of questions to tell you what kind of animal you could be 
+//This program will ask you a series of questions to tell you what kind of animal you could be
 
 using system;
 class twentyquestions{
@@ -11,10 +11,13 @@ class twentyquestions{
   {
     Console.WriteLine ("Would you rather eat plants or animals?");
     A = command.ToUpper (Console.ReadLine ());
-    If (A == "PLANTS")
+    if (A == "PLANTS")
     {
       Console.WriteLine ("Would you rather be big or small?");
-      a = command.ToUpper ();
+      a = command.ToUpper (Console.ReadLine ());
+        if (a == "BIG")
+        Console.WriteLine ();
+        else
     }
     else{
 
@@ -24,23 +27,32 @@ class twentyquestions{
   {
     Console.WriteLine ("Would you rather eat plants or animals?");
     B = command.ToUpper (Console.ReadLine ());
-    If (B == "PLANTS")
+    if (B == "PLANTS")
     {
       Console.WriteLine ("Would you rather be big or small?");
-      b = command.ToUpper ();
+      b = command.ToUpper (Console.ReadLine ());
+        if (b == "BIG")
+        Console.WriteLine ("You Are A Duck!");
+        else
+        Console.WriteLine ("You are a Humming Bird!");
     }
     else{
-
+      Console.WriteLine ("Would you rather be big or small?");
+      b = command.ToUpper (Console.ReadLine ());
+        if (b == "BIG")
     }
   }
   public static void Water ()
   {
     Console.WriteLine ("Would you rather eat plants or animals?");
     C = command.ToUpper (Console.ReadLine ());
-    If (C == "PLANTS")
+    if (C == "PLANTS")
     {
       Console.WriteLine ("Would you rather be big or small?");
-      c = command.ToUpper ();
+      c = command.ToUpper (Console.ReadLine ());
+        if (b == "BIG")
+        Console.WriteLine ();
+        else
     }
     else{
 
@@ -49,14 +61,20 @@ class twentyquestions{
   public static void main (){
     Console.WriteLine ("Would you like to Fly, Swim, or Run?");
     A = command.ToUpper (Console.ReadLine ());
-    switch ()
+    switch (A)
     {
       case "FLY":
           handleAir ();
+          break;
       case "RUN":
           handleLand ();
+          break;
       case "SWIM":
           handleWater ();
+          break;
+      default:
+          Console.WriteLine("Invalid option");
+          break;
     }
   }
 }
